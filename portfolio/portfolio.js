ea8c5bf682f3ba0bde9ef3f9affbe9011030081a
Eric Chen <ericnlchen@gmail.com>
2025-07-23 21:59:24 -0700

    re-implement content scale with css transforms

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,14 +1,10 @@
 // Ensure that L-ink video loads first
 
-// Resize slides
 const sectionContainer = document.getElementsByClassName("slide-container")[0];
-const viewportWidth = window.innerWidth;
 let sections = document.getElementsByTagName("section");
-const sectionWidth = sections[0].offsetWidth;
-if (sectionWidth > viewportWidth) {
-  const scaleFactor = (0.8 * viewportWidth) / sectionWidth;
-  sectionContainer.style.zoom = scaleFactor;
-}
+const outer = document.getElementsByClassName("outer")[0];
+const sectionHeight = 1080; // px
+const sectionVerticalMargin = 30; // px
 
 // Add page numbers
 let pageNum = 0;
@@ -20,13 +16,38 @@ for (const section of sections) {
   pageNum++;
 }
 
-// Add indicators for new projects
-for (const section of sections) {
-  if (section.classList.contains("proj-title")) {
-    const bar = document.createElement("div");
-    bar.setAttribute("background-color", "black");
-    bar.setAttribute("height", "20px");
-    bar.setAttribute("width", "1000px");
-    section.appendChild(bar);
+// Resize slides
+const resizeSlides = () => {
+  const viewportWidth = window.innerWidth;
+  const sectionWidth = sections[0].offsetWidth;
+  if (sectionWidth > viewportWidth) {
+    const scaleFactor = (0.8 * viewportWidth) / sectionWidth;
+
+    sectionContainer.style.transform = `scale(${scaleFactor})`;
+
+    sectionContainer.style.height = `${
+      (sectionHeight + sectionVerticalMargin) * sections.length
+    }px`;
+
+    outer.style.height = `${
+      (sectionHeight + sectionVerticalMargin) * sections.length * scaleFactor
+    }px`;
   }
-}
+};
+
+resizeSlides();
+
+window.addEventListener("resize", () => {
+  resizeSlides();
+});
+
+// // Add indicators for new projects
+// for (const section of sections) {
+//   if (section.classList.contains("proj-title")) {
+//     const bar = document.createElement("div");
+//     bar.setAttribute("background-color", "black");
+//     bar.setAttribute("height", "20px");
+//     bar.setAttribute("width", "1000px");
+//     section.appendChild(bar);
+//   }
+// }
