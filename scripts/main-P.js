1d6fe31d3c50aa4594aee73459d8157d4f038a61
Eric Chen <ericnlchen@gmail.com>
2025-10-16 16:02:39 -0700

    implement lsys drawing functionality

---
+++ scripts/main-P.js
@@ -0,0 +1,8 @@
+export function mainP() {
+    const canvas = document.getElementById("background-canvas");
+    const psHandle = document.getElementsByClassName("ps-handle")[0];
+    psHandle.addEventListener("mousedown", () => {
+        console.log('ps')
+    })
+
+}
